c9b29c911ac7d1c9d6c94b622e1dac4e1905bfef
Rui <rui.diogo.costa@hotmail.com>
2020-12-08 15:27:03 +0000

    update database config

--- config/database.js
+++ config/database.js
@@ -4,7 +4,8 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        uri: "${process.env.DATABASE_URI}",
+        uri:
+          "mongodb://skeyeDB:SkeyeDBPass@cluster0-shard-00-00.rmsvc.mongodb.net:27017,cluster0-shard-00-01.rmsvc.mongodb.net:27017,cluster0-shard-00-02.rmsvc.mongodb.net:27017/<dbname>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
       },
       options: {
         ssl: true,
