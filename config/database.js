dce8f384e038b9f38beda04668a2dd4d15149dac
Rui <rui.diogo.costa@hotmail.com>
2020-12-08 15:03:18 +0000

    update database config

--- config/database.js
+++ config/database.js
@@ -1,19 +1,14 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'SkeyeDB'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        uri: "${process.env.DATABASE_URI}",
+        database: "${process.env.DATABASE_NAME}",
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: true,
       },
     },
   },
