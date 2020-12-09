09b2157171ec6c0b1fa8c604476d9bb1fbe75578
Rui <rui.diogo.costa@hotmail.com>
2020-12-09 10:54:02 +0000

    Adds direct DB connection

--- config/database.js
+++ config/database.js
@@ -1,11 +1,27 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "mongoose",
+//       settings: {
+//         uri: `${process.env.DATABASE_URI}`,
+//         database: `${process.env.DATABASE_NAME}`,
+//       },
+//       options: {
+//         ssl: true,
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
       settings: {
-        uri: `${process.env.DATABASE_URI}`,
-        database: `${process.env.DATABASE_NAME}`,
+        uri:
+          "mongodb://skeyeDB:SkeyeDBPass@cluster0-shard-00-00.rmsvc.mongodb.net:27017,cluster0-shard-00-01.rmsvc.mongodb.net:27017,cluster0-shard-00-02.rmsvc.mongodb.net:27017/skeyeDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
       },
       options: {
         ssl: true,
