54d5f6eae99e717f35b01c6cd2f57aace2b0887b
Rui <rui.diogo.costa@hotmail.com>
2020-12-09 15:01:21 +0000

    Adds notification yolo post request

--- api/notification/controllers/notification.js
+++ api/notification/controllers/notification.js
@@ -9,26 +9,33 @@ const { seed } = require("../../classtype/controllers/classtype");
 
 module.exports = {
   async postYolo(ctx) {
-    var user = ctx.idUser;
-    var camera = ctx.idCamera;
-    var timestamp = ctx.timestamp;
-    var classes = ctx.Classes;
-    var url = ctx.urlVideo;
+    var user = ctx.request.body.idUser;
+    var camera = ctx.request.body.idCamera;
+    var timestamp = ctx.request.body.timestamp;
+    var classes = ctx.request.body.Classes;
+    var url = ctx.request.body.urlVideo;
     var cam = await strapi.api.camera.services.camera.findOne({
       _id: camera,
     });
-    for(classe of classes){
-      for(classeType of cam.classType) {
-        if(classe = classeType.Class.description) {
+    console.log(classes);
+    for (var classe of classes) {
+      for (var classeType of cam.classtypes) {
+        const classObj = await strapi.services.class.findOne({
+          description: classe,
+        });
+        const classDesc = classObj.description;
+        console.log(`comparing ${classDesc} with ${classe}`);
+        if (classe === classDesc) {
           var obj = {
             detectionDate: timestamp,
             videoLink: url,
             seen: false,
-            notificationType: classeType.NotificationType,
+            classtype: classeType,
             Camera: camera,
           };
           await strapi.services.notification.create(obj);
-        }
+          return "success";
+        } else return "Class not found";
       }
     }
   },
@@ -38,15 +45,15 @@ module.exports = {
       var dectDate = obj.detectionDate;
       var vidLink = obj.videoLink;
       var seen = obj.seen;
-      var notiType = obj.NotificationType;
+
       var cam = await strapi.api.camera.services.camera.findOne({
         _id: obj.Camera,
       });
+
       var obj = {
         detectionDate: dectDate,
         videoLink: vidLink,
         seen: seen,
-        notificationType: notiType,
         Camera: cam,
       };
       await strapi.services.notification.create(obj);
